382151b1fedec1bd81ff7bba5a9c1d2b1c82468a
Mumbaikar007 <brainteaser97@gmail.com>
2017-12-14 15:05:34 +0530

    Almost There ...

--- HandGests.py
+++ HandGests.py
@@ -1,4 +1,5 @@
 
+
 import cv2
 import numpy as np
 import math
@@ -29,18 +30,18 @@ while(cap.isOpened()):
                cv2.CHAIN_APPROX_NONE)
 
 
-    cnt = max(contours, key = lambda x: cv2.contourArea(x))
+    hand_contour = max(contours, key = lambda x: cv2.contourArea(x))
 
 
-    x, y, w, h = cv2.boundingRect(cnt)
+    x, y, w, h = cv2.boundingRect(hand_contour)
     cv2.rectangle(hand_window, (x, y), (x+w, y+h), (0, 0, 255), 0)
 
 
     # finding convex hull
-    hull = cv2.convexHull(cnt, returnPoints=False)
+    hull = cv2.convexHull(hand_contour, returnPoints=False)
 
     # finding convexity defects
-    defects = cv2.convexityDefects(cnt, hull)
+    defects = cv2.convexityDefects(hand_contour, hull)
 
 
     count_defects = 0
@@ -49,43 +50,41 @@ while(cap.isOpened()):
     for i in range(defects.shape[0]):
         s,e,f,d = defects[i,0]
 
-        start = tuple(cnt[s][0])
-        end = tuple(cnt[e][0])
-        far = tuple(cnt[f][0])
+        start = tuple(hand_contour[s][0])
+        end = tuple(hand_contour[e][0])
+        far = tuple(hand_contour[f][0])
 
-        # find length of all sides of triangle
+        
         a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
         b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
         c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
 
-        # apply cosine rule here
+        
         angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
 
-        # ignore angles > 90 and highlight rest with red dots
+        
         if angle <= 90:
             count_defects += 1
             cv2.circle(hand_window, far, 1, [0,0,255], -1)
-        #dist = cv2.pointPolygonTest(cnt,far,True)
+        
 
-        # draw a line from start to end i.e. the convex points (finger tips)
-        # (can skip this part)
         cv2.line(hand_window,start, end, [0,255,0], 2)
-        #cv2.circle(hand_window,far,5,[0,0,255],-1)
 
-    # define actions required
+
+
     if count_defects == 1:
-        cv2.putText( frame,"", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, ( 0, 0, 255), 2)
+        cv2.putText( frame,"I am Pranav", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, ( 0, 0, 255), 2)
     elif count_defects == 2:
-        cv2.putText( frame, "", (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, ( 0, 255, 0), 2)
+        cv2.putText( frame, "I love food", (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, ( 0, 255, 0), 2)
     elif count_defects == 3:
-        cv2.putText( frame,"", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, ( 0, 255, 0), 2)
+        cv2.putText( frame,"4", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, ( 0, 255, 0), 2)
     elif count_defects == 4:
-        cv2.putText( frame,"", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, ( 0, 255, 0), 2)
+        cv2.putText( frame,"Hi", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, ( 0, 255, 0), 2)
     else:
-        cv2.putText( frame,"", (50, 50),\
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+        cv2.putText( frame,"Hello World", (50, 50),\
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, ( 0, 255, 0), 2)
 
-    # show appropriate images in windows
+
     cv2.imshow('Gesture', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
